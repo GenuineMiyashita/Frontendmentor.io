855538d9101e2956291a54262deada0852aaa3e0
GenuineMiyashita <JosueRAnes@proton.me>
2023-04-21 16:36:31 -0400

    Separate Items Array
    Separated items into it's own file for scalability and to reduce clutter on main code.

---
+++ src/components/Items.jsx
@@ -0,0 +1,38 @@
+import Roxas from "../assets/Roxas_01.jpeg";
+import Axel from "../assets/Axel_01.jpeg";
+import Xion from "../assets/Xion_01.jpg";
+
+const Items = [
+  {
+    id: 1,
+    img: { Roxas },
+    category: "ルームフレグランス",
+    name: "ロクサス",
+    description:
+      "プチ グレインのグリーンをイメージしたシトラス アコードから、クローバーの甘さが広がり、ラストはムスクやアンバー、バルサム ノートが夏の暑さを感じさせる香り。",
+    price: "7500円",
+    salePrice: "5,940円",
+  },
+  {
+    id: 2,
+    img: { Axel },
+    category: "ルームフレグランス",
+    name: "アクセル",
+    description:
+      "アップルとシトラスのフレッシュな香り立ちから、ジャスミンとピーチの甘みが広がり、ラストはマイルドなムスクとアンバーが温かみのあるオーラで包み込んでくれるような香りです。",
+    price: "7500円",
+    salePrice: "5,940円",
+  },
+  {
+    id: 3,
+    img: { Xion },
+    category: "ルームフレグランス",
+    name: "シオン",
+    description:
+      "グレープフルーツのフレッシュな香り立ちから、微かなティーノートが広がり、ラストはしっとりとしたシーソルトとバルサミックなラブダナムが夏の終わりの夕暮れの切ない思い出を感じさせる香りです。",
+    price: "7500円",
+    salePrice: "5,940円",
+  },
+];
+
+export default Items;
