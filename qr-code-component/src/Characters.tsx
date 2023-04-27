380f9bc7f7d08fcfe368f60b7d08289b4a4a8cee
GenuineMiyashita <JosueRAnes@proton.me>
2023-04-27 12:43:06 -0400

    Add Characters
    Added an array of characters from the game to cycle through with buttons.

---
+++ qr-code-component/src/Characters.tsx
@@ -0,0 +1,39 @@
+const Characters = [
+  {
+    id: 1,
+    rarity: 5,
+    name: "Trailblazer",
+    description:
+      "In the beginning of the game, Kafka with the help of Silver Wolf activated their receptacle, awakening them to the real world where they woke up inside the Herta Space Station during the invasion of the Antimatter Legion.",
+  },
+  {
+    id: 2,
+    rarity: 5,
+    name: "Seele",
+    description:
+      "A spirited and valiant key member of Wildfire who grew up in the perilous Underworld of Belobog.",
+  },
+  {
+    id: 3,
+    rarity: 5,
+    name: "Bronya",
+    description:
+      "She is the commander of the Silvermane Guards and the heir to the Supreme Guardian of Belobog.",
+  },
+  {
+    id: 4,
+    rarity: 5,
+    name: "Gepard",
+    description:
+      "He is the captain of the Silvermane Guards who belongs to the noble Landau family in Belobog, and is responsible for the city's defenses and maintains peace.",
+  },
+  {
+    id: 5,
+    rarity: 5,
+    name: "Clara",
+    description:
+      "She is a shy young girl orphaned at an early age, accompanied by an ancient mech named Svarog. Her greatest wish is 'to have a family'.",
+  },
+];
+
+export default Characters;
