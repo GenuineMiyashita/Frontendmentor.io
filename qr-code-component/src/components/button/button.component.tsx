676c4247e872519d899e71b7c3b1610d31b58d9d
GenuineMiyashita <JosueRAnes@proton.me>
2023-04-27 12:42:48 -0400

    Add Button Component
    Started work on button component for the next and prev buttons.

---
+++ qr-code-component/src/components/button/button.component.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useState } from "react";
+import Characters from "../../Characters";
+import "./button.styles.css";
+
+const Button = () => {
+  return (
+    <>
+      <button>Click Me</button>
+    </>
+  );
+};
+
+export default Button;
