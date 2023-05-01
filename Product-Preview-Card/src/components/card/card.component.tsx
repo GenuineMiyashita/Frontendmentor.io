bad7b56e054202e73aa8f4cd24f99a95c1478b9e
JosueAnes <JosueRAnes@proton.me>
2023-05-01 15:13:15 -0400

    Update Buttons
    Updated Button functionality and allowed to be cycled through all items in the array.

--- Product-Preview-Card/src/components/card/card.component.tsx
+++ Product-Preview-Card/src/components/card/card.component.tsx
@@ -1,24 +1,22 @@
 import { useState } from "react";
-import products from "../Items";
-import cartImg from "../../assets/icon-cart.svg";
+import Products from "../Items";
+import CheckoutCart from "../../assets/icon-cart.svg";
 import "./card.styles.css";
 
 const Card = () => {
-  const [items, setItems] = useState(products[0]);
-
-  const handlePrevItem = () => {
-    setItems(products[0]);
-  };
-
-  const handleNextItem = () => {
-    setItems(products[1]);
+  const [selectedItem, setSelectedItem] = useState(0);
+  const currentItem = Products[selectedItem];
+  const incrementItem = (val: number) => {
+    setSelectedItem((currentVal: number) => {
+      return Math.min(Math.max(val + currentVal, 0), Products.length);
+    });
   };
 
   return (
     <main>
       <div className="container" id="mainContainer">
         <div className="rounded-t-lg" id="imgContainer">
-          {items.img}
+          {currentItem.img}
         </div>
 
         <div
@@ -29,18 +27,18 @@ const Card = () => {
             className="pt-2 font-montserrat text-sm font-medium tracking-[.35em]"
             id="itemCategory"
           >
-            {items.category}
+            {currentItem.category}
           </p>
 
           <h1 className="pt-2 font-fraunces text-3xl font-bold" id="itemName">
-            {items.name}
+            {currentItem.name}
           </h1>
 
           <p
             className="pt-2 font-montserrat text-sm font-medium"
             id="itemDescription"
           >
-            {items.description}
+            {currentItem.description}
           </p>
 
           <div id="priceContainer">
@@ -51,7 +49,7 @@ const Card = () => {
               aria-hidden="true"
               id="itemSalePrice"
             >
-              {items.salePrice}
+              {currentItem.salePrice}
 
               {/* Screen reader price */}
               <span className="sr-only">Original: 55.93</span>
@@ -60,32 +58,35 @@ const Card = () => {
                 className="ml-4 align-middle font-montserrat text-sm line-through"
                 aria-hidden="true"
               >
-                {items.price}
+                {currentItem.price}
               </span>
             </p>
+            {/* <button
+              className="my-2 w-full rounded-xl bg-gray-700 py-1 hover:bg-gray-800"
+              id="checkoutBtn"
+            >
+              <img src={CheckoutCart} className="mr-4 inline" alt="..." />
+              <span className="inline font-montserrat text-white">
+                Checkout
+              </span>
+            </button> */}
+
             <div className="flex justify-evenly py-1">
               <button
                 className="rounded bg-gray-300 px-2 font-bold text-gray-800 hover:bg-gray-400"
-                onClick={handlePrevItem}
+                disabled={selectedItem <= 0}
+                onClick={() => incrementItem(-1)}
               >
                 &larr; Prev
               </button>
               <button
                 className="rounded bg-gray-300 px-2 font-bold text-gray-800 hover:bg-gray-400"
-                onClick={handleNextItem}
+                disabled={selectedItem >= 2}
+                onClick={() => incrementItem(1)}
               >
                 Next &rarr;
               </button>
             </div>
-            <button
-              className="my-2 w-full rounded-xl bg-gray-700 py-1 hover:bg-gray-800"
-              id="checkoutBtn"
-            >
-              <img src={cartImg} className="mr-4 inline" alt="..." />
-              <span className="inline font-montserrat text-white">
-                Checkout
-              </span>
-            </button>
           </div>
         </div>
       </div>
