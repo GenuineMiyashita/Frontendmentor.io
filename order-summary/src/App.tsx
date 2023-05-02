62a05a6c7ed70460982fb8d6d27c8975c390e13f
JosueAnes <JosueRAnes@proton.me>
2023-05-02 15:13:25 -0400

    Update CSS

--- order-summary/src/App.tsx
+++ order-summary/src/App.tsx
@@ -6,7 +6,10 @@ function App() {
     <>
       <main>
         <div id="mainContainer" className="w-80">
-          <div id="musicContainer" className="text-sm text-center mb-1">
+          <div
+            id="musicContainer"
+            className="text-sm text-center mb-1 rounded-md bg-slate-100/25"
+          >
             Sample Our Collection By Clicking The Image!
           </div>
           <div
@@ -48,18 +51,18 @@ function App() {
                 <p className="font-semibold text-sm mb-1">Annual Plan</p>
                 <p className="font-light text-xs">$59.99/year</p>
               </div>
-              <a href="" className="self-center text-sm">
-                Change
-              </a>
+              <button className="self-center text-sm">Change</button>
             </div>
             <div
               id="paymentContainer"
               className="flex flex-col text-center pb-6"
             >
-              <button className="w-64 py-2 mb-4 mx-auto rounded-md font-bold bg-slate-600 shadow-md text-white">
+              <button className="w-64 py-2 mb-4 mx-auto rounded-md font-bold bg-slate-400 hover:bg-slate-600 hover:text-gray-200 shadow-md text-gray-900">
                 Proceed to Payment
               </button>
-              <button className="text-sm font-light">Cancel Order</button>
+              <button className="text-sm font-light hover:font-bold ">
+                Cancel Order
+              </button>
             </div>
           </div>
         </div>
