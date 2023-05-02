f3ab025a0e4efe820fadb801c17412a59ce2231c
JosueAnes <JosueRAnes@proton.me>
2023-05-02 13:37:59 -0400

    Update App
    Added base layout of the project with minor CSS to center.

--- order-summary/src/App.tsx
+++ order-summary/src/App.tsx
@@ -1,25 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div id="mainContainer" className="w-80">
+        <div id="imgContainer">
+          <img src={require("./assets/Hero.jpg")} alt="..." />
+        </div>
+        <div id="textContainer">
+          <div id="heroText">
+            <h1>Order Summary</h1>
+            <p>
+              You can now listen to millions of songs, audiobooks, and podcasts
+              on any device anywhere you like!
+            </p>
+          </div>
+          <div id="subscriptionContainer">
+            <img src={require("./assets/icon-music.svg")} alt="" />
+            <div>
+              <p className="">Annual Plan</p>
+              <p>$59.99/year</p>
+            </div>
+            <button>Change</button>
+          </div>
+          <div id="paymentContainer">
+            <button>Proceed to Payment</button>
+            <button>Cancel Order</button>
+          </div>
+        </div>
+      </div>
+      <footer>
+        Image Credit: Nairu on{" "}
+        <a href="https://www.pixiv.net/en/users/93459916">Pixiv</a>
+      </footer>
+    </main>
   );
 }
 
