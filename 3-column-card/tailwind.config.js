5fb43e72716308eb25933238aa89374b4a2eec3c
JosueAnes <JosueRAnes@proton.me>
2023-05-03 16:34:43 -0400

    Update Tailwind Config
    Updated file paths.

--- 3-column-card/tailwind.config.js
+++ 3-column-card/tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        archivistBackground: "url('../src/assets/archivist.jpg')",
+        enchanterBackground: "url('../src/assets/enchanter.jpg')",
+        prayermakerBackground: "url('../src/assets/prayermaker.jpg')",
+      },
+      fontFamily: {
+        Marcellus: ["Marcellus", "sans-serif"],
+        Nunito: ["Nunito Sans", "sans-serif"],
+      },
+      colors: {
+        btnColor: "#352D34",
+      },
+    },
   },
   plugins: [],
 };
