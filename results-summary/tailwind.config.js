ba0a368f4954e11bdb21524e7fffa70ed12af412
JosueAnes <JosueRAnes@proton.me>
2023-05-04 14:32:23 -0400

    Add Google Fonts

--- results-summary/tailwind.config.js
+++ results-summary/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        oswald: ["Oswald", "sans serif"],
+        source: ["Source Sans Pro", "sans serif"],
+      },
+    },
   },
   plugins: [],
 };
