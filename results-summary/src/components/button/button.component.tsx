4f47068518a30e6b751eef9a050b6fef087bfc05
JosueAnes <JosueRAnes@proton.me>
2023-05-05 15:22:31 -0400

    Update Button Component

--- results-summary/src/components/button/button.component.tsx
+++ results-summary/src/components/button/button.component.tsx
@@ -1,5 +1,29 @@
-const Button = ({ onClick }: any) => {
-  return <button onClick={onClick}>Add</button>;
+import { useState } from "react";
+
+const Button = () => {
+  const [statNumber, setStatNumber] = useState(0);
+
+  function handleAddStat() {
+    setStatNumber(statNumber + 1);
+  }
+
+  function handleRemoveStat() {
+    setStatNumber(statNumber - 1);
+  }
+  return (
+    <>
+      <button onClick={handleRemoveStat} disabled={statNumber <= 0}>
+        &larr;
+      </button>
+      <p className="inline px-3">
+        {statNumber}
+        <span className="pl-2">/ 9</span>
+      </p>
+      <button onClick={handleAddStat} disabled={statNumber >= 9}>
+        &rarr;
+      </button>
+    </>
+  );
 };
 
 export default Button;
