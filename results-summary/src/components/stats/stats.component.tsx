cf5b2ee30a45417ca78cf46587fa418728d979b6
JosueAnes <JosueRAnes@proton.me>
2023-05-05 15:21:58 -0400

    Create Stats Component

---
+++ results-summary/src/components/stats/stats.component.tsx
@@ -0,0 +1,30 @@
+import Attributes from "../../attributes";
+import Button from "../button/button.component";
+
+const StatList = () => {
+  const character = Attributes.map((attribute) => {
+    return (
+      <li
+        id="stats"
+        key={attribute.id}
+        className="flex justify-between my-1 px-2 py-1.5 rounded-md bg-gradient-to-r from-slate-200 to-slate-300"
+      >
+        <div>
+          {" "}
+          {attribute.img} {attribute.name}
+        </div>
+        <div>
+          <Button />
+        </div>
+      </li>
+    );
+  });
+
+  return (
+    <div>
+      <ul className="w-56 mx-auto">{character}</ul>
+    </div>
+  );
+};
+
+export default StatList;
