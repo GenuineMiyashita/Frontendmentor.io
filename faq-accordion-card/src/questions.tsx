a97bfde148a97f035411a89c1923fff143d346b4
JosueAnes <JosueRAnes@proton.me>
2023-05-11 15:09:01 -0400

    Create Questions Array

---
+++ faq-accordion-card/src/questions.tsx
@@ -0,0 +1,32 @@
+const QuestionsAnswers = [
+  {
+    id: 1,
+    question: "How many team members can I invite?",
+    answer: "You can have an unlimited amount of team members!",
+  },
+  {
+    id: 2,
+    question: "What is the maximum file upload size?",
+    answer:
+      "No more than 2GB. All files in your account must fit your alloted storage space.",
+  },
+  {
+    id: 3,
+    question: "How do I reset my password?",
+    answer: ` You can reset your password by clicking on the "forgot password"
+          button under login.`,
+  },
+  {
+    id: 4,
+    question: "Can I cancel my subscription?",
+    answer: `Yes! You can cancel at anytime under the "subscription" section
+          in account settings.`,
+  },
+  {
+    id: 5,
+    question: " Do you provide additional support?",
+    answer: "Yes! You can contact our support team 24/7 at hi@support.com",
+  },
+];
+
+export default QuestionsAnswers;
