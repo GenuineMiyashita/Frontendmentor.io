21ec9a37980fa2b179b4784a1b66f42b4f158d30
JosueAnes <JosueRAnes@proton.me>
2023-05-12 15:34:04 -0400

    Add Google Fonts

--- workit-landing-page/tailwind.config.js
+++ workit-landing-page/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Fraunces: ["Fraunces", "sans-serif"],
+        Manrope: ["Manrope", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
